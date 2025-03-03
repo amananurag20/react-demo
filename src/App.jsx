2759844f711f6be5826d859e26bbd51398cb17c5
aman anurag <amananurag.20@gmail.com>
2025-03-03 11:24:05 +0530

    children props

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
       <Product name={"Samsung"} price={200000} />
       <Product name={"Oppo"} price={90000} />
       <Product name={"OnePlus"} price={60000} />
+      <Product />
+      <Product name="vivo">hello how are you</Product>
     </div>
   );
 };
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,14 @@
 import "./Product.css";
 
 const Product = (props) => {
+  console.log(props);
   return (
     <>
       <h1 style={{ backgroundColor: "red", color: "white" }}>
         product- {props.name}
       </h1>
       <h1 className="price">price- {props.price}</h1>
+      <p>{props.children}</p>
     </>
   );
 };
