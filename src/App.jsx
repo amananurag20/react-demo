87c832af61aca1df437a45808df48a98794e479d
aman anurag <amananurag.20@gmail.com>
2025-03-04 10:54:49 +0530

    local storage

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import HomePage from "./components/HomePage/HomePage";
 import Product from "./components/Product/Product";
 const App = () => {
-  const [count, setCount] = useState(1000); //[1000,fxn]
+  const [count, setCount] = useState(() => {
+    const storedCount = localStorage.getItem("count");
+    if (storedCount) {
+      return storedCount;
+    }
+    return 1000;
+  });
 
   const handleClick = () => {
-    setCount(count + 1000); //2000
-    setCount((prev) => prev + 1000); //3000
-    setCount(count + 1000); //2000
-
-    console.log(count); //1000
+    setCount(count + 1000);
+    localStorage.setItem("count", count + 1000);
   };
 
   return (
