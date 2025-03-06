d55a3a782db2ae2df4afaf96a51618a03adb4ca8
aman anurag <amananurag.20@gmail.com>
2025-03-06 11:32:23 +0530

    added e-commerce

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   const apiCall = async () => {
     const response = await fetch("https://fakestoreapi.com/products");
     const json = await response.json();
-    setData(json);
+    setData(json.slice(0, 10));
   };
   useEffect(() => {
     apiCall();
@@ -15,9 +15,42 @@ const App = () => {
   console.log(data);
 
   return (
-    <div>
-      <h1>count- {count}</h1>
-      {data[0]?.price}
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "10px",
+        justifyContent: "space-around",
+      }}
+    >
+      {data.map((item) => (
+        <div
+          style={{
+            width: "300px",
+            background: "cyan",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <h2>{item.title}</h2>
+          <img src={item.image} alt="dasdsa" style={{ width: "200px" }}></img>
+          <h3>Price rs {item.price}</h3>
+          <button
+            style={{
+              padding: "20px",
+              background: "red",
+              fontWeight: "bold",
+              color: "white",
+              borderRadius: "5px",
+            }}
+            onClick={() => alert("Item added to cart")}
+          >
+            Add to Cart
+          </button>
+        </div>
+      ))}
       <button onClick={() => setCount(count + 1000)}>Increase</button>
     </div>
   );
