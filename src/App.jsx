e6505b44b95f060afd8a3ab473a22657c1f9873f
aman anurag <amananurag.20@gmail.com>
2025-03-11 10:41:09 +0530

    form event

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ const App = () => {
     email: "",
     mobile: "",
     address: "",
-    DOB: "",
   });
 
   const [passwordType, setPasswordType] = useState("password");
@@ -31,34 +30,80 @@ const App = () => {
     }
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    console.log("form submitted");
+    let name = e.target[0].value;
+    let password = e.target[1].value;
+    let email = e.target[1].value;
+
+    console.log(name, password, email);
+  };
+
   return (
     <>
       <div className="container">
-        <div>
-          <p>Enter your name</p>
-          <input
-            type="text"
-            placeholder="enter your name"
-            onChange={handleChange}
-            value={input.name}
-            name="name"
-          ></input>
-          <h1>{name}</h1>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <p>Enter your name</p>
+            <input
+              type="text"
+              placeholder="enter your name"
+              onChange={handleChange}
+              value={input.name}
+              name="name"
+            ></input>
+            <h1>{name}</h1>
+          </div>
 
-        <div>
-          <p>Password</p>
-          <input
-            type={passwordType}
-            placeholder="enter your password"
-            onChange={handleChange}
-            value={input.password}
-            name="password"
-          ></input>
-          <button onClick={handlePasswordView}>
-            {passwordType === "password" ? "view password" : "hide password"}
+          <div>
+            <p>Password</p>
+            <input
+              type={passwordType}
+              placeholder="enter your password"
+              onChange={handleChange}
+              value={input.password}
+              name="password"
+            ></input>
+            <button onClick={handlePasswordView}>
+              {passwordType === "password" ? "view password" : "hide password"}
+            </button>
+            <p>Enter your Email</p>
+            <input
+              type="text"
+              placeholder="enter your Email"
+              onChange={handleChange}
+              value={input.email}
+              name="email"
+            ></input>
+            <p>Enter your mobile no.</p>
+            <input
+              type="text"
+              placeholder="enter your mobile no."
+              onChange={handleChange}
+              value={input.mobile}
+              name="mobile"
+            ></input>
+            <p>Enter your address</p>
+            <input
+              type="text"
+              placeholder="your address"
+              onChange={handleChange}
+              value={input.address}
+              name="address"
+            ></input>
+          </div>
+          <br></br>
+          <button
+            type="submit"
+            onClick={() => {
+              console.log("button submit");
+            }}
+          >
+            submit the form
           </button>
-        </div>
+        </form>
       </div>
     </>
   );
