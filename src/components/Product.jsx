bb648f3ae50c8a07ef84ed72e63d6a2ac8b13e94
aman anurag <amananurag.20@gmail.com>
2025-03-19 16:53:08 +0530

    routing

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,16 +4,6 @@ import { Link } from "react-router-dom";
 const Product = () => {
   return (
     <div>
-      <div className="p-4 bg-red-600 flex justify-between text-white">
-        <h1 className="font-bold">React Routing</h1>
-
-        <div className="flex gap-4 font-semibold">
-          <a href="/">Home</a>
-          <a href="/product">Product</a>
-          <Link to={"/mobile"}>Mobile</Link>
-          <Link to={"/login"}>Login</Link>
-        </div>
-      </div>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRULpYuPcas5DFbICFFgawoEEdeOPuUm9lI2g&s"></img>
     </div>
   );
