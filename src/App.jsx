88ce174beb54e6028c92b6a9e8a3cf048cfcdbc2
aman anurag <amananurag.20@gmail.com>
2025-03-20 10:36:03 +0530

    nested route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Mobile from "./components/Mobile";
 import Navbar from "./components/Navbar";
+import User from "./components/User";
 
 const App = () => {
   return (
@@ -14,9 +15,12 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/:mobile/data/:id" element={<Mobile />} />
-          <Route path="/product" element={<Product />} />
+
+          <Route path="/product" element={<Product />}>
+            <Route path="/product/user" element={<User />} />
+            <Route path="/product/login" element={<Login />} />
+          </Route>
+          <Route path="/:id" element={<Mobile />} />
           <Route path="/cat" element={<h1 className="text-7xl">hiii</h1>} />
           <Route path="*" element={<h1>Page not found 404 </h1>} />
         </Routes>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Product = () => {
   return (
     <div>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRULpYuPcas5DFbICFFgawoEEdeOPuUm9lI2g&s"></img>
+      <Outlet />
     </div>
   );
 };
