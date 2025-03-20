fa529035b3e3e543986daa2189fd89dda7ce41cd
aman anurag <amananurag.20@gmail.com>
2025-03-20 10:57:00 +0530

    routing

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,6 +5,10 @@ const Product = () => {
   return (
     <div>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRULpYuPcas5DFbICFFgawoEEdeOPuUm9lI2g&s"></img>
+
+      <Link to={"/product/user"}>
+        <button>Move to user page</button>
+      </Link>
       <Outlet />
     </div>
   );
